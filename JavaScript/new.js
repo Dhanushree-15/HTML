094d58a0de592ac1fb387969aadefe95a8d47f20
Dhanushree-15 <dhanushree.1516@gmail.com>
2025-04-16 23:37:59 +0530

    javascript

---
+++ JavaScript/new.js
@@ -0,0 +1,33 @@
+var age=10
+age='gudgehhhe'
+
+let gender='male'
+gender=562442
+
+const institue='x-workz'
+
+
+let emp1={id:12,employeeName:'anu',salary:450000}
+
+let emp2= new Object();
+emp2.id=13;
+emp2.employeeName='huud';
+emp2.salary=567899;
+
+function emp3(id,employeeName,salary){
+    this.id=id;
+    this.employeeName=employeeName;
+    this.salary=salary;
+}
+
+let obj3=emp3(satisfies,satisfies);
+
+console.log(age)
+console.log(emp1)
+console.log(emp2)
+console.log(emp3)
+
+//key:value--id:12
+
+
+//document.writeln(age)
